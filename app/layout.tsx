77755d074bbefb11e7880d58634de500b8f5e12b
Diyorbek G'aybullayev <gaybullayevdiyorbek2001@gmail.com>
2025-05-23 14:51:19 +0500

    add 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,45 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "DIYORCODER  |  Diyorbek G'aybullayev - Web Developer ",
+  description: "Explore the personal portfolio of Diyorbek G'aybullayev — a skilled frontend developer, freelancer, and mentor specializing in React and modern web technologies.",
+  keywords: [
+    "Diyorbek G'aybullayev",
+    "Frontend Developer Uzbekistan",
+    "React Developer",
+    "Portfolio",
+    "Vercel portfolio",
+    "JavaScript Developer",
+    "Next.js portfolio",
+  ],
+  authors: [{ name: "Diyorbek G'aybullayev", url: "https://diyorcoder.uz" }],
+  creator: "Diyorbek G'aybullayev",
+  robots: "index, follow",
+  openGraph: {
+    title: "DIYORCODER  |  Diyorbek G'aybullayev - Web Developer",
+    description: "Frontend developer, freelancer and mentor from Uzbekistan. View my work and contact me today.",
+    url: "https://diyorcoder.uz",
+    siteName: "Diyor Coder",
+    images: [
+      {
+        url: "https://diyorcoder.uz/og-image.jpg", // Saytingizda mavjud rasm bo‘lishi kerak
+        width: 1200,
+        height: 630,
+        alt: "Diyor Coder Portfolio Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Diyor Coder | Frontend Developer Portfolio",
+    description: "Check out Diyorbek's projects, skills and contact info.",
+    creator: "@diyorcoder", // Agar Twitter bor bo‘lsa
+    images: ["https://diyorcoder.uz/og-image.jpg"],
+  },
 };
+
 
 export default function RootLayout({
   children,
--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import HeroContent from "../sub/HeroContent";
 
 const Hero = () => {
+
+  
+  
   return (
     <div className="relative flex flex-col h-full w-full" id="about-me">
       <video
